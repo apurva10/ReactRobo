1b435fe85268a4ccea963cf3d8e9ebeeec16b091
apurva10 <apurva.sonak@gmail.com>
2020-04-15 21:23:52 +0200

    Updates

---
+++ precache-manifest.0530c3ceda5175c2946b275779f9711e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8892b71c4d6385798985d835d6c73aa7",
+    "url": "/ReactRobo/index.html"
+  },
+  {
+    "revision": "eecc029dc02b03d1547f",
+    "url": "/ReactRobo/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "998eb2a5f008007d1826",
+    "url": "/ReactRobo/static/css/main.e3d0e943.chunk.css"
+  },
+  {
+    "revision": "eecc029dc02b03d1547f",
+    "url": "/ReactRobo/static/js/2.a52f469c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactRobo/static/js/2.a52f469c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "998eb2a5f008007d1826",
+    "url": "/ReactRobo/static/js/main.bd3e4038.chunk.js"
+  },
+  {
+    "revision": "b4990f18632bfe07c3f0",
+    "url": "/ReactRobo/static/js/runtime-main.02afb3ff.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/ReactRobo/static/media/SEGA.82e09f78.TTF"
+  }
+]);
